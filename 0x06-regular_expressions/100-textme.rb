425e995c2b0f0615add68077c697d6c92e448775
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-01-31 18:59:31 -0800

    mess

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+mess=""
+mess+=puts ARGV[0].scan(/[from:.*?]/).join
+mess+=(',')
+mess+=puts ARGV[0].scan(/[to:.*?]/).join
+mess+=(',')
+mess+=puts ARGV[0].scan(/[flags:.*?]/).join
+puts mess
