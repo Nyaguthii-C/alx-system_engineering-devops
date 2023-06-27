167967b53e107e51328150d9fa6e9ca1a394bb5d
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-06-27 16:37:58 -0700

    use puppet to set up configuration

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# using Puppet to make changes to configuration file to connect to a server without typing a password
+
+file_line {' declare identity fiile':
+  ensure => 'present',
+  path   => '/root/.ssh',
+  line   => 'IdentifyFile ~/.ssh/school',
+}
+
+file_line {'refuse authentication using password':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
