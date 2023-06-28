02429b5fb9876db9b1d718e134314866bbc1b43a
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-06-28 11:50:28 -0700

    add stdlib to puppet manifest for file_line

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,14 @@
 # using Puppet to make changes to configuration file to connect to a server without typing a password
+include stdlib
 
-file_line {' declare identity fiile':
-  ensure => 'present',
+file_line { ' declare identity file':
+  ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentifyFile ~/.ssh/school',
 }
 
-file_line {'refuse authentication using password':
-  ensure => 'present',
+file_line { 'refuse authentication using password':
+  ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
 }
