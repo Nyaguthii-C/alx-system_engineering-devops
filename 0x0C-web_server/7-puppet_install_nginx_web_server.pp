e7e3f12f993a5d99dafd5d058e3271d59e6d0add
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-07-08 07:05:21 -0700

    apply above commands using puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,36 +1,25 @@
-#install Nginx
-class nginx {
-  package { 'nginx':
-    ensure => installed,
-  }
-#start Nginx
+# install nginx using puppet and configure to listen on port 80, redirect  and return hello world
 
-  service { 'nginx':
-    ensure  => running,
-    enable  => true,
-    require => Package['nginx'],
-  }
-#redirection, port config, page
-
-  file { '/etc/nginx/sites-enabled/default':
-    ensure  => present,
-    content => '
-      server {
-        listen 80;
-        server_name _;
-
-        location / {
-          return 301 https://help.dreamhost.com/hc/en-us/articles/216456087-Creating-redirects-with-Nginx;
-        }
-
-        location /hello{
-	  return 200 "Hello World!";
-	}
-      }
-    ',
-    notify  => Service['nginx'],
-  }
+package {'nginx':
+  ensure => 'present',
 }
 
-include nginx
+exec {'install':
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  provider => shell,
 
+}
+
+exec {'Hello':
+  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+  provider => shell,
+}
+
+exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:help.dreamhost.com\/hc\/en-us\/articles\/216456087-Creating-redirects-with-Nginx\/;\\n\\t}/" /etc/nginx/sites-available/default':
+  provider => shell,
+}
+
+exec {'run':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+}
