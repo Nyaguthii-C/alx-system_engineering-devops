6e1546d352c7f64b17d1d770997a9031bf8a55b5
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-07-16 14:14:33 -0700

    configure using puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,4 @@
 # using Puppet to make changes to configuration file to connect to a server without typing a password
-include stdlib
-
 file_line { ' declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
