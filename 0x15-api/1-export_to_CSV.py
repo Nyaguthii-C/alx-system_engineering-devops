af42f6be4873538406dcd327b0c339ce0fe15a26
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-07-26 19:20:05 -0700

    fetch the correct attributes

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,10 +8,12 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    USER_ID = sys.argv[1]
+    USER_ID_select = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/users"
+    USER_ID_url = url + "/{}".format(USER_ID_select)
+    USER_ID = requests.get(USER_ID_url).json().get("id")
     USERNAME_url = url + "/{}".format(USER_ID)
-    USERNAME = requests.get(USERNAME_url).json().get("name")
+    USERNAME = requests.get(USERNAME_url).json().get("username")
     tasks_url = url + "/{}/todos".format(USER_ID)
     tasks = requests.get(tasks_url).json()
     with open('{}.csv'.format(USER_ID), 'w', newline='') as csvfile:
