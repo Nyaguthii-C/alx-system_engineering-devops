29485c25475ab76635602c483d446d0cb4773083
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-08-14 16:42:11 -0700

    add condition to ensure idempotence in script

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,12 @@
 # puppet file to configure server to accept 2000 requests
+# onlyif to check whether the current ULIMIT value
+# is different from desired value, Idempotence
 
 exec {'replace' :
   provider => shell,
-  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 2000\"/" /etc/default/nginx'
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 2000\"/" /etc/default/nginx',
+  onlyif   => "grep -qE '^ULIMIT=\"-n .*\"$' /etc/default/nginx",
+  before   => Exec['restart'],
 }
 
 exec {'restart':
